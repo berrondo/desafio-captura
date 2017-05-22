fc1afe8808f4c9507dfa0ffeb9dfd1f5f01cac58
Claudio Berrondo <claudio.berrondo@gmail.com>
2017-05-22 19:55:19 -0300

    para o exists, fetchone basta

--- db.py
+++ db.py
@@ -8,19 +8,24 @@ c = con.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS links (nome text, titulo text, url text UNIQUE, subdepartamento text)''')
 c.execute('''CREATE TABLE IF NOT EXISTS web (link text UNIQUE, html text, callback text)''')
 
+
 def insert(nome, titulo, url, subdepartamento):
     c.execute("INSERT INTO links VALUES (?, ?, ?, ?)", (nome, titulo, url, subdepartamento))
     con.commit()
 
+
 def select():
     return c.execute('SELECT * from links').fetchall()
 
+
 def exists(url):
-    return c.execute('SELECT * from links where url=?', (url, )).fetchall()
+    return c.execute('SELECT * from links where url=?', (url, )).fetchone()
+
 
 def delete():
     c.execute('DELETE FROM links')
     con.commit()
+
 
 def close():
     con.close()
